9d6632558fc88aee53f5bdffbc3e21b1661e7219
Tejas <last44705@gmail.com>
2022-08-09 21:26:03 -0500

    few more

--- basic_math.py
+++ basic_math.py
@@ -1,65 +1,92 @@
-####### is a number prime? #######
-# def is_prime(number):
-#     if (int(number) > 1):
-#         for i in range(2, int(number)):
-#             if(int(number) % i == 0):
-#                 return True
-#     else:
-#         return False
+###### is a number prime? #######
+def is_prime(number):
+    if (int(number) > 1):
+        for i in range(2, int(number)):
+            if(int(number) % i == 0):
+                return True
+    else:
+        return False
 
 
-# print("enter #: ")
-# n = input()
-# if(is_prime(n)):
-#     print("Neither prime nor composite.")
-# else:
-#     print("Prime")
-###################################
+print("enter #: ")
+n = input()
+if(is_prime(n)):
+    print("Neither prime nor composite.")
+else:
+    print("Prime")
+##################################
 
 
-####### is it a leap year? #######
-# def is_leap_year(year):
-#     if(year % 4 == 0) and (year % 100 != 0) or (year % 400 == 0):
-#         print("leap year.")
-#         return
-#     print("not a leap year")
+###### is it a leap year? #######
+def is_leap_year(year):
+    if(year % 4 == 0) and (year % 100 != 0) or (year % 400 == 0):
+        print("leap year.")
+        return
+    print("not a leap year")
 
 
-# is_leap_year(2001)
-###################################
+is_leap_year(2001)
+##################################
 
 
-####### multiplication table #######
-# def multiplication_table_of(number):
-#     for i in range(11):
-#         print(number, "x", i, "=", number * i)
+###### multiplication table #######
+def multiplication_table_of(number):
+    for i in range(11):
+        print(number, "x", i, "=", number * i)
 
 
-# multiplication_table_of(79)
-###################################
+multiplication_table_of(79)
+##################################
 
 
-####### HCF #######
-# def find_hcf(num1, num2):
-#     smaller_number = num1 if num1 < num2 else num2
-#     for i in range(1, smaller_number+1):
-#         if (num1 % i == 0 and num2 % i == 0):
-#             hcf = i
-#     return hcf
+###### HCF #######
+def find_hcf(num1, num2):
+    smaller_number = num1 if num1 < num2 else num2
+    for i in range(1, smaller_number+1):
+        if (num1 % i == 0 and num2 % i == 0):
+            hcf = i
+    return hcf
 
 
-# print(find_hcf(54, 24))
-###################################
+print(find_hcf(54, 24))
+##################################
 
 
-####### HCF using euclidean algorithm #######
-# def hcf_euclidean(x, y):
-#     rem = x % y  # 6
-#     while(rem):
-#         x, y = y, rem
-#         rem = x % y
-#     return y
+###### HCF using euclidean algorithm #######
+def hcf_euclidean(x, y):
+    rem = x % y  # 6
+    while(rem):
+        x, y = y, rem
+        rem = x % y
+    return y
 
 
-# print(hcf_euclidean(300, 400))
-###################################
+print(hcf_euclidean(300, 400))
+##################################
+
+
+###### sum of user inputs #######
+def sum_of_inputs():
+    sum = 0
+    while(1):
+        number = input("Enter a number: ")
+        if number == 'x':
+            break
+        sum += int(number)
+    return sum
+
+
+print(sum_of_inputs())
+##################################
+
+
+###### sum of user inputs #######
+def is_odd_number(x):
+    return True if int(x) % 2 != 0 else False
+
+
+if(is_odd_number(input("Enter a number: "))):
+    print("Odd")
+else:
+    print("Even")
+##################################
