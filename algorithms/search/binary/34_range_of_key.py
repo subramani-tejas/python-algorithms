e5adebe0aae35b40d3445fa96a42192197658ee8
Tejas <last44705@gmail.com>
2022-09-22 03:14:18 -0500

    leetcode 34 - search range

---
+++ algorithms/search/binary/34_range_of_key.py
@@ -0,0 +1,33 @@
+# leetcode34
+# https://leetcode.com/problems/find-first-and-last-position-of-element-in-sorted-array/
+from typing import List
+
+
+def searchRange(self, arr: List[int], key: int) -> List[int]:
+    first_occurence = binary_search(arr, key, first_index=True)
+    last_occurence = binary_search(arr, key, first_index=False)
+
+    return [first_occurence, last_occurence]
+
+
+def binary_search(arr, key, first_index):
+    start, end = 0, len(arr) - 1
+    ans = -1
+
+    while start <= end:
+        mid = start + ((end - start) // 2)
+
+        if key == arr[mid]:
+            # potential ans
+            ans = mid
+            if first_index:
+                end = mid - 1
+            else:
+                start = mid + 1
+
+        elif key > arr[mid]:
+            start = mid + 1
+        else:
+            end = mid - 1
+
+    return ans
--- algorithms/search/binary/simple_binary.py
+++ algorithms/search/binary/simple_binary.py
@@ -28,16 +28,16 @@ print("comparisons:", comparisons)
 
 # leetcode 744
 # https://leetcode.com/problems/find-smallest-letter-greater-than-target/
-class Solution:
-    def nextGreatestLetter(self, letters: List[str], target: str) -> str:
-        start, end = 0, len(letters) - 1
 
-        while start <= end:
-            mid = start + (end - start) // 2
+def nextGreatestLetter(self, letters: List[str], target: str) -> str:
+    start, end = 0, len(letters) - 1
 
-            if target < letters[mid]:
-                end = mid - 1
-            else:
-                start = mid + 1
+    while start <= end:
+        mid = start + (end - start) // 2
 
-        return letters[start % len(letters)]
+        if target < letters[mid]:
+            end = mid - 1
+        else:
+            start = mid + 1
+
+    return letters[start % len(letters)]
