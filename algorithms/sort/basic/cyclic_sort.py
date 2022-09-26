925945aa1b53980395134a1552cc8a3495766fd0
Tejas <last44705@gmail.com>
2022-09-25 20:02:59 -0500

    cyclic sort - corrections

--- algorithms/sort/basic/cyclic_sort.py
+++ algorithms/sort/basic/cyclic_sort.py
@@ -7,8 +7,8 @@ def cyclic_sort(arr):
     i = 0
 
     while i < len(arr):
-        correct_index = arr[i] - 1
-        if i != correct_index:
+        correct_index = arr[i]
+        if arr[i] != arr[correct_index]:
             swap(arr, i, correct_index)
         else:
             i += 1
@@ -16,5 +16,5 @@ def cyclic_sort(arr):
     return arr
 
 
-arr = [3, 5, 2, 1, 4]
+arr = [3, 0, 1]
 print(cyclic_sort(arr))
--- algorithms/sort/leetcode/268_missing_number.py
+++ algorithms/sort/leetcode/268_missing_number.py
@@ -1,31 +1,16 @@
-from typing import List
+class Solution:
+    def missingNumber(self, nums: List[int]) -> int:
+        i = 0
 
+        while i < len(nums):
+            correct_i = nums[i]
+            if nums[i] < len(nums) and nums[i] != nums[correct_i]:
+                nums[i], nums[correct_i] = nums[correct_i], nums[i]
+            else:
+                i += 1
 
-def missingNumber(nums: List[int]) -> int:
-    arr = cyclic_sort(nums)
-    for i in range(0, len(arr)):
-        if i != arr[i]:
-            return i
-    return -1
+        for i in range(0, len(nums)):
+            if i != nums[i]:
+                return i
 
-
-def cyclic_sort(arr):
-    i = 0
-
-    while i < len(arr):
-        correct_index = arr[i]
-        if i == correct_index or correct_index >= len(arr):
-            i += 1
-        elif i != correct_index:
-            swap(arr, i, correct_index)
-
-    return arr
-
-
-def swap(arr, i, j):
-    arr[i], arr[j] = arr[j], arr[i]
-    return arr
-
-
-arr = [3, 0, 1]
-print(missingNumber(nums=arr))
+        return len(nums)
