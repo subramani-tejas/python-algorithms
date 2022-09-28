da0078c3bbfaf630802837f4833ee72a353b0ed8
Tejas <last44705@gmail.com>
2022-09-27 21:25:45 -0500

    expert - shifted bin search

---
+++ expert/shifted_binary_search.py
@@ -0,0 +1,46 @@
+def shiftedBinarySearch(arr, key):
+    pivot_index = find_pivot_index(arr)
+
+    if pivot_index == -1:
+        return binary_search(arr, key, 0, len(arr) - 1)
+
+    if arr[pivot_index] == key:
+        return pivot_index
+
+    search_left = binary_search(arr, key, 0, pivot_index - 1)
+    if search_left == -1:
+        return binary_search(arr, key, pivot_index + 1, len(arr) - 1)
+    else:
+        return search_left
+
+
+def find_pivot_index(arr):
+    start, end = 0, len(arr) - 1
+
+    while start <= end:
+        mid = start + (end - start) // 2
+
+        if mid < end and arr[mid] > arr[mid + 1]:
+            return mid
+        if mid > start and arr[mid] < arr[mid - 1]:
+            return mid - 1
+        if arr[mid] <= arr[start]:
+            end = mid - 1
+        else:
+            start = mid + 1
+
+    return -1
+
+
+def binary_search(arr, key, start, end):
+    while start <= end:
+        mid = start + (end - start) // 2
+
+        if key == arr[mid]:
+            return mid
+        elif key > arr[mid]:
+            start = mid + 1
+        else:
+            end = mid - 1
+
+    return -1
