05f8161a8ca217c630a2f72b99ced67301cf3238
Tejas <last44705@gmail.com>
2023-04-28 22:33:39 -0500

    day 8 - end

---
+++ my_sclr/day-8/leaders.py
@@ -0,0 +1,38 @@
+# Find the number of leaders in the array
+# Leader: An element is greater than all the elements on its left side
+# a[i] > a[0, i-1]
+
+# O(N^2)
+def initial_find_leaders(arr):
+    n = len(arr)
+    leader = False
+    count = 0
+
+    for i in range(n):
+        leader = True
+        for j in range(i):
+            # not a leader
+            if arr[j] >= arr[i]:
+                leader = False
+        if leader:
+            count += 1
+
+    return count
+
+
+# O(N)
+def optimized_find_leaders(arr):
+    n = len(arr)
+    max_so_far = arr[0]
+    count = 1
+
+    for i in range(1, n):
+        if arr[i] > max_so_far:
+            max_so_far = arr[i]
+            count += 1
+
+    return count
+
+
+arr = [2, 5, 3, 4, 17, 16]
+optimized_find_leaders(arr)
