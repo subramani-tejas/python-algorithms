d91f31cecba4fe13e717dcaa58d86fd011a353a5
Tejas <last44705@gmail.com>
2023-05-06 23:50:10 -0500

    kk-assign 01 - contd

--- kk_assignments/src/flow_of_program_01/main.py
+++ kk_assignments/src/flow_of_program_01/main.py
@@ -1,25 +1,27 @@
-class Main:
+# Take a number as input and print the multiplication table for it.
+def get_multiplication_table(num):
+    res = []
+    for i in range(1, 11):
+        # print(f"{num} x {i} = {num * i}")
+        res.append(num * i)
+    return res
 
-    # Take a number as input and print the multiplication table for it.
-    def get_multiplication_table(self, num):
-        res = []
-        for i in range(1, 11):
-            # print(f"{num} x {i} = {num * i}")
-            res.append(num * i)
-        return res
 
-    # Input a year and find whether it is a leap year or not.
-    def is_leap_year(self, year):
-        pass
+# Input a year and find whether it is a leap year or not.
+def is_leap_year(year):
+    return year % 4 == 0 and (year % 400 == 0 or year % 100 != 0)
 
-    # Take 2 numbers as inputs and find their HCF.
-    def find_HCF(self, x, y):
-        pass
 
-    # Take 2 numbers as inputs and find their LCM.
-    def find_LCM(self, x, y):
-        pass
+# Take 2 numbers as inputs and find their HCF.
+def find_HCF(x, y):
+    pass
 
-    # Keep taking numbers as inputs till the user enters ‘x’, after that print sum of all.
-    def print_sum_of_inputs(self):
-        pass
+
+# Take 2 numbers as inputs and find their LCM.
+def find_LCM(x, y):
+    pass
+
+
+# Keep taking numbers as inputs till the user enters ‘x’, after that print sum of all.
+def print_sum_of_inputs():
+    pass
--- kk_assignments/tests_package/flow_of_program_01/test_main.py
+++ kk_assignments/tests_package/flow_of_program_01/test_main.py
@@ -1,7 +1,7 @@
 import io
 import sys
 import unittest
-import kk_assignments.src.flow_of_program_01.main as main_module
+import kk_assignments.src.flow_of_program_01.main as source
 
 
 class TestMain(unittest.TestCase):
@@ -9,7 +9,12 @@ class TestMain(unittest.TestCase):
     def test_get_multiplication_table(self):
         num = 5
         expected_output = [5, 10, 15, 20, 25, 30, 35, 40, 45, 50]
-        main = main_module.Main()
-        captured_output = main.get_multiplication_table(num)
+        captured_output = source.get_multiplication_table(num)
         assert captured_output == expected_output
-        print("test_print_multiplication_table passed OK")
+        print("print_multiplication_table() passed OK")
+
+    def test_is_leap_year(self):
+        assert source.is_leap_year(1900) == False
+        assert source.is_leap_year(2000) == True
+        assert source.is_leap_year(2020) == True
+        print("is_leap_year() passed OK")
