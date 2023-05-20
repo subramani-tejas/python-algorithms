a14aa4e0f5fe4ebf5ea8e0971b10962b18f9bcf4
tejas <last47705@gmail.com>
2023-05-20 16:37:53 -0500

    smaller_numbers_than_current using prefix sum

--- kk_assignments/src/week_005_arrays/easy_set.py
+++ kk_assignments/src/week_005_arrays/easy_set.py
@@ -121,3 +121,66 @@ def num_identical_pairs(nums):
             count_ += freq[item]
 
     return count_
+
+
+def smaller_numbers_than_current(arr):
+    """ LeetCode: 1365. How Many Numbers Are Smaller Than the Current Number Given the
+    array nums, for each nums[i] find out how many numbers in the array are smaller than it.
+    That is, for each nums[i] you have to count the number of valid j's such that
+    j != i and nums[j] < nums[i]. Return the answer in an array.
+
+    Example 1:
+    Input: nums = [8,1,2,2,3]
+    Output: [4,0,1,1,3]
+    Explanation:
+    For nums[0]=8 there exist four smaller numbers than it (1, 2, 2 and 3).
+    For nums[1]=1 does not exist any smaller number than it.
+    For nums[2]=2 there exist one smaller number than it (1).
+    For nums[3]=2 there exist one smaller number than it (1).
+    For nums[4]=3 there exist three smaller numbers than it (1, 2 and 2).
+
+    Example 2:
+    Input: nums = [6,5,4,8]
+    Output: [2,1,0,3]
+
+    Example 3:
+    Input: nums = [7,7,7,7]
+    Output: [0,0,0,0]
+    """
+
+    """ Using prefix sum | count sort | TC: O(N + max_element) | SC: O(N)
+    Count the frequency of each element in the array and then calculate
+    the prefix sum of the frequency array. The prefix sum array will give you
+    the number of elements smaller than the current element.
+    """
+    n = len(arr)
+    res = [0] * n
+    freq = [0] * (max(arr) + 1)
+
+    for element in arr:
+        freq[element] += 1
+
+    freq_prefix_sum = [freq[0]]
+    for i in range(1, len(freq)):
+        freq_prefix_sum.append(freq[i] + freq_prefix_sum[i - 1])
+
+    print(freq_prefix_sum)
+
+    for i in range(n):
+        if arr[i] != 0:
+            res[i] = freq_prefix_sum[arr[i] - 1]
+
+    return res
+
+    # noinspection PyUnreachableCode
+    """ Brute force. O(N^2) 
+            n = len(arr)
+            res = [0] * n
+        
+            for i in range(n):
+                for j in range(n):
+                    if arr[j] < arr[i]:
+                        res[i] += 1
+        
+            return res
+        """
